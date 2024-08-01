f676fdf80cb47c8459739497770a33d312a811ed
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-08-01 10:55:08 +0200

    space-evenly

--- src/hero.jsx
+++ src/hero.jsx
@@ -1,16 +1,4 @@
-import close from "../public/close.svg";
-import open from "../public/open.svg";
-
 function Header() {
-
-    const closebtn = () => {
-        alert("closed ");
-    };
-
-    const openbtn = () =>{
-        alert("opend ");
-    };
-
     return (
         <div className="header-background">
             <nav>
@@ -19,9 +7,8 @@ function Header() {
                     <li><a href="#">ABOUT</a></li>
                     <li><a href="#">ARTICLES</a></li>
                 </ul>
-                <button type="button">Subscribe</button>
+                <button >Subscribe</button>
             </nav>
-
             <h2 class="Hero-header">DRONOTICZ</h2>
             <h3 class="Hero-subheader">THE ERA OF DRONES</h3>
             <div class="rectangle"></div>
--- src/rectangle.jsx
+++ src/rectangle.jsx
@@ -9,25 +9,33 @@ function Rectangle() {
             <h3 className="section-header ">LARTEST ARTICLES</h3>
             <div className="rectangle" ></div>
             <div className="card-grid">
-                <div className="card">
-                    <img src="./drone4.jpg" className="in-card" />
-                    <div className="rectangle-in-card"></div>
-                    <p className="in-card-apdding">How To automate Survaillance drone</p>
+                <div className="section-container">
+                    <div className="card">
+                        <img src="./drone4.jpg" className="in-card" />
+                        <div className="rectangle-in-card"></div>
+                        <p className="in-card-apdding">How To automate Survaillance drone</p>
+                    </div>
                 </div>
-                <div className="card">
-                    <img src="./drone3.jpg" className="in-card" />
-                    <div className="rectangle-in-card"></div>
-                    <p className="in-card-apdding">Robotics and Artificaila Intellingence</p>
+                <div className="section-container">
+                    <div className="card">
+                        <img src="./drone3.jpg" className="in-card" />
+                        <div className="rectangle-in-card"></div>
+                        <p className="in-card-apdding">Robotics and Artificial Intelligence</p>
+                    </div>
                 </div>
-                <div className="card">
-                    <img src="./drone1.jpg" className="in-card" />
-                    <div className="rectangle-in-card"></div>
-                    <p className="in-card-apdding">Improving Lives Through Robotics Prosthetics</p>
+                <div className="section-container">
+                    <div className="card">
+                        <img src="./drone1.jpg" className="in-card" />
+                        <div className="rectangle-in-card"></div>
+                        <p className="in-card-apdding">Improving Lives Through Robotics Prosthetics</p>
+                    </div>
                 </div>
-                <div className="card">
-                    <img src="/drone2.jpg" className="in-card" />
-                    <div className="rectangle-in-card"></div>
-                     <p className="in-card-apdding">Power of Robotics in Agriculture</p>
+                <div className="section-container">
+                    <div className="card">
+                        <img src="/drone2.jpg" className="in-card" />
+                        <div className="rectangle-in-card"></div>
+                        <p className="in-card-apdding">Power of Robotics in Agriculture</p>
+                    </div>
                 </div>
             </div>
            
