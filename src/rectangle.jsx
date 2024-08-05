c6b4eaf782542557e94a452fd528b7e67c3b6776
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-08-05 08:06:44 +0200

    Update rectangle.jsx

--- src/rectangle.jsx
+++ src/rectangle.jsx
@@ -1,38 +1,42 @@
 import "@fontsource/inter";
 import "@fontsource/inter/400.css";
+import drone4 from "../public/drone4.jpg";
+import drone3 from "../public/drone3.jpg";
+import drone1 from "../public/drone1.jpg";
+import drone2 from "../public/drone2.jpg";
 
 function Rectangle() {
 
     return (
 
         <div className="card-box">
-            <h3 className="section-header ">LARTEST ARTICLES</h3>
+            <h3 className="section-header ">LATEST ARTICLES</h3>
             <div className="rectangle-5" ></div>
             <div className="card-grid">
                 <div className="section-container">
                     <div className="card">
-                        <img src="./drone4.jpg" className="in-card" />
+                        <img src={drone4} className="in-card" />
                         <div className="rectangle-in-card"></div>
                         <p className="in-card-apdding">How To automate Survaillance drone</p>
                     </div>
                 </div>
                 <div className="section-container">
                     <div className="card">
-                        <img src="./drone3.jpg" className="in-card" />
+                        <img src={drone3} className="in-card" />
                         <div className="rectangle-in-card"></div>
                         <p className="in-card-apdding">Robotics and Artificial Intelligence</p>
                     </div>
                 </div>
                 <div className="section-container">
                     <div className="card">
-                        <img src="./drone1.jpg" className="in-card" />
+                        <img src={drone1} className="in-card" />
                         <div className="rectangle-in-card"></div>
                         <p className="in-card-apdding">Improving Lives Through Robotics Prosthetics</p>
                     </div>
                 </div>
                 <div className="section-container">
                     <div className="card">
-                        <img src="/drone2.jpg" className="in-card" />
+                        <img src={drone2} className="in-card" />
                         <div className="rectangle-in-card"></div>
                         <p className="in-card-apdding">Power of Robotics in Agriculture</p>
                     </div>
